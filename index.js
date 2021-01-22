d1608ca6cde93abe026a3895690ba415f552996d
azizarx <50118704+azizarx@users.noreply.github.com>
2021-01-22 22:09:14 +0100

    sec

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
+const {prefix} = require('./config.json');
 client.login(process.env.TOKEN);
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);    
   });
-
-
-      client.on('message', msg =>{
-  try {
-      if((msg.author.bot == false)&&(msg.content == '=ticket test')&&(msg.author.id == '372142246331416579')){
-        const embed = new Discord.MessageEmbed()
-      .setColor('#54d1a5')
-      .setTitle('Ticket :smile:')
-      .addField('React to open a ticket','🎫')
-      
-      msg.channel.send(embed).then(async embed =>{
-       await embed.react('🎫')
-    });
-      msg.delete();
-      }
-    } 
-    catch (error) {
-        console.log(error);
-        msg.react("❌");
+ client.on('message', msg =>{
+   if(msg.content[0]== prefix){
+    let command = msg.content.split(' ')[0].slice(1);
+    switch(command){
+    case'ping':
+      msg.channel.send('pong');
+      break;
     }
-      });
+  }
+  });
