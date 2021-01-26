2c4612e3ec72c54f41137c321f98b5ea10165605
azizarx <50118704+azizarx@users.noreply.github.com>
2021-01-26 20:04:29 +0100

    bla bla

--- commands.js
+++
@@ -1,6 +0,0 @@
-const {prefix} = require('./config.json');
-module.exports = (client,aliases,callback)=>{
-    client.on('message',msg =>{
-      const {content} = msg;
-    });
-}
--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const api = require('imageapi.js');
 require('dotenv').config();
 const {prefix} = require('./config.json');
 client.login(process.env.TOKEN);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);    
   });
- client.on('message', msg =>{
+ client.on('message', async msg =>{
    if(msg.content[0]== prefix){
+     let subreddit = msg.content.split(' ')[1] 
     let command = msg.content.split(' ')[0].slice(1);
     switch(command){
     case'ping':
       msg.channel.send('pong');
+      console.log(msg.content)
+      break;
+      case 'meme': 
+      let img = await api(subreddit)
+      const Embed = new Discord.MessageEmbed()
+      .setTitle(`A meme from r/${subreddit}`)
+      .setURL(`https://www.reddit.com/r/${subreddit}`)
+      .setColor('RANDOM')
+      .setImage(img)
+      .setFooter('Joe mama is gay.')
+      msg.channel.send(Embed);
       break;
     }
+    
   }
   });
+
+
+
+
+
+
+
+
+
