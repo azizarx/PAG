95e77e2b77c7c593b6be52f7a388d9032c0b1704
azizarx <50118704+azizarx@users.noreply.github.com>
2021-08-19 22:30:37 +0100

    lols1
    lols

--- commands.js
+++ commands.js
@@ -1,10 +1,13 @@
-const {prefix} = require('./config.json');
+const prefix = '$'
+const db = require('./fireStore');
 let ping = require('./commands/ping');
 let meme = require('./commands/meme');
 let commands = {ping,meme};
 module.exports = async (msg) =>{
+    if (msg.author.bot) return;
+	if (!msg.content.startsWith(prefix)) return;
     let args = msg.content.split(" ")
-    let command = args.shift()
+    let command = args.shift();
     if (command.charAt(0)==prefix){
         command = command.substring(1).toLowerCase()
         commands[command](msg,args)
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const db = require('./fireStore');
 require('dotenv').config();
 client.login(process.env.TOKEN);
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);    
-  });
-  let commandHandler = require('./commands')
- client.on('message', commandHandler);
-
-
-
-
-
-
-
-
+client.on('ready',async () => {
+ await console.log(`Logged in as ${client.user.tag}!`);
+});
+let commandHandler = require('./commands')
+ client.on('message',commandHandler);
+client.on('guildCreate',async gData =>{
+     ownername = client.users.cache.find(a =>a.id == gData.ownerID)
+    db.collection('guilds').doc(gData.name).set({
+        'guildID' : gData.id,
+        'guildName' : gData.name,
+        'guildOwner' : ownername.username,
+        'guildOwnerID' : gData.ownerID,
+        'guildMemberCount' : gData.memberCount,
+        'prefix' : '$'
+    });
+});
